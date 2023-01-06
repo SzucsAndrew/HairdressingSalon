9b81248799ca3dcef32e825b4b4811e2063cd012
szucsandr3w <szucsandr3w@gmail.com>
2023-01-06 11:05:53 +0100

    Added comment

--- HairdressingSalon.Data/HairdresserDbContextFactory.cs
+++ HairdressingSalon.Data/HairdresserDbContextFactory.cs
@@ -3,14 +3,15 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace HairdressingSalon.Data
 {
-    public class HairdresserDbContextFactory : IDesignTimeDbContextFactory<HairdressingSalonDbContext>
-    {
-        public HairdressingSalonDbContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<HairdressingSalonDbContext>();
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HairdressingSalon;Trusted_Connection=True");
+    //For fix scaffolding
+    //public class HairdresserDbContextFactory : IDesignTimeDbContextFactory<HairdressingSalonDbContext>
+    //{
+    //    public HairdressingSalonDbContext CreateDbContext(string[] args)
+    //    {
+    //        var optionsBuilder = new DbContextOptionsBuilder<HairdressingSalonDbContext>();
+    //        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HairdressingSalon;Trusted_Connection=True");
 
-            return new HairdressingSalonDbContext(optionsBuilder.Options);
-        }
-    }
+    //        return new HairdressingSalonDbContext(optionsBuilder.Options);
+    //    }
+    //}
 }
